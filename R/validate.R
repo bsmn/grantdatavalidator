93dfca2aae529637fa3dd54915f692659b9a8284
kdaily <kenneth.daily@sagebase.org>
2019-02-20 14:19:07 -0800

    rename.

--- R/validate.R
+++ R/validate.R
@@ -9,21 +9,21 @@ validate <- function(manifestsviewid, parentid) {
   nichddatarow <- submissiondata %>%
     dplyr::filter(nda_short_name == "nichd_btb02")
 
-  subjectdata <- read_and_validate(id = subjectdatarow$id,
-                                   version = subjectdatarow$currentVersion,
-                                   validation_func = validate_subject_data)
+  subjectdata <- syn_get_and_validate_manifest(id = subjectdatarow$id,
+                                               version = subjectdatarow$currentVersion,
+                                               validation_func = validate_subject_data)
 
-  nichddata <- read_and_validate(id = nichddatarow$id,
-                                 version = nichddatarow$currentVersion,
-                                 validation_func = validate_nichd_data,
-                                 subjectdata = subjectdata)
+  nichddata <- syn_get_and_validate_manifest(id = nichddatarow$id,
+                                             version = nichddatarow$currentVersion,
+                                             validation_func = validate_nichd_data,
+                                             subjectdata = subjectdata)
 
-  sampledata <- read_and_validate(id = sampledatarow$id,
-                                  version = sampledatarow$currentVersion,
-                                  validation_func = validate_sample_data,
-                                  submissiondata = submissiondata,
-                                  subjectdata = subjectdata,
-                                  nichddata = nichddata)
+  sampledata <- syn_get_and_validate_manifest(id = sampledatarow$id,
+                                              version = sampledatarow$currentVersion,
+                                              validation_func = validate_sample_data,
+                                              submissiondata = submissiondata,
+                                              subjectdata = subjectdata,
+                                              nichddata = nichddata)
 
   return(list(submission = submissiondata,
               sampledata = sampledata,
@@ -36,15 +36,23 @@ get_submission <- function(manifestsviewid, parentid) {
   query <- glue::glue("select * from {manifestsviewid} where parentId=\'{parentid}\'")
   dres <- synapser::synTableQuery(query)
   submissiondata <- dres$asDataFrame() %>%
-    validate_manifests_submission() %>%
-    dplyr::arrange(nda_short_name)
+    validate_manifests_submission()
   return(submissiondata)
 }
 
 #' @export
-read_and_validate <- function(id, version, validation_func, ...) {
+read_manifest <- function(filepath) {
+  readr::read_csv(filepath, skip = 1, col_types = readr::cols(.default = "c"))
+}
+
+syn_get_manifest <- function(id, version) {
   dataobj <- synapser::synGet(id, version = version)
-  readr::read_csv(dataobj$path, skip = 1) %>%
+  read_manifest(filepath = dataobj$path)
+}
+
+#' @export
+syn_get_and_validate_manifest <- function(id, version, validation_func, ...) {
+  syn_get_manifest(id, version) %>%
     validation_func(., ...)
 }
 
@@ -63,7 +71,7 @@ validate_manifests_submission <- function(data) {
     assertr::verify(assertr::not_na(grant)) %>%
     assertr::verify(dplyr::n_distinct(grant) == 1) %>%
     assertr::verify(nda_short_name %in% expected_nda_short_names) %>%
-    assertr::chain_end() %>%
+    assertr::chain_end(error_fun = assertr::error_df_return) %>%
     tibble::as_tibble()
 }
 
@@ -75,7 +83,7 @@ validate_subject_data <- function(data) {
     assertr::verify(assertr::is_uniq(src_subject_id)) %>%
     assertr::verify(assertr::is_uniq(sample_id_original)) %>%
     assertr::verify(assertr::is_uniq(sample_id_biorepository)) %>%
-    assertr::chain_end() %>%
+    assertr::chain_end(error_fun = assertr::error_df_return) %>%
     tibble::as_tibble()
 }
 
@@ -86,7 +94,7 @@ validate_nichd_data <- function(data, subjectdata) {
     assertr::verify(assertr::is_uniq(sample_id_original)) %>%
     assertr::verify(subjectkey %in% subjectdata$subjectkey) %>%
     assertr::verify(src_subject_id %in% subjectdata$src_subject_id) %>%
-    assertr::chain_end() %>%
+    assertr::chain_end(error_fun = assertr::error_df_return) %>%
     tibble::as_tibble()
 }
 
@@ -100,6 +108,6 @@ validate_sample_data <- function(data, submissiondata, subjectdata, nichddata) {
     assertr::verify(subjectkey %in% subjectdata$subjectkey) %>%
     assertr::verify(src_subject_id %in% subjectdata$src_subject_id) %>%
     assertr::verify(sample_id_biorepository %in% nichddata$sample_id_original) %>%
-    assertr::chain_end() %>%
+    assertr::chain_end(error_fun = assertr::error_df_return) %>%
     tibble::as_tibble()
 }
